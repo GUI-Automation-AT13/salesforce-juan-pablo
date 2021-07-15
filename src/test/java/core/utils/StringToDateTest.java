bed1c160138f37d03e67ba9eca5ec762a005f29d
Juan Pablo Gonzales <juampi7237@gmail.com>
2021-07-15 09:04:32 -0400

    vchnage hange test

--- src/test/java/core/utils/StringToDateTest.java
+++ src/test/java/core/utils/StringToDateTest.java
@@ -15,21 +15,16 @@ import static org.testng.Assert.*;
 
 public class StringToDateTest {
 
-    private StringToDate calendar;
+    private StringToDate calendar ;
     private Calendar newCalendar;
     private Date newActualDate;
-    SimpleDateFormat dateFormat;
-
-    @BeforeTest
-    public void setup() {
-        calendar = new StringToDate();
-        newCalendar = Calendar.getInstance();
-        newActualDate = newCalendar.getTime();
-        dateFormat = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
-    }
+    SimpleDateFormat dateFormat= new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");;
 
     @Test
     public void testToday() {
+        calendar = new StringToDate();
+        newCalendar = Calendar.getInstance();
+        newActualDate = newCalendar.getTime();
         String actual = calendar.convertirString("TODAY");
         String expected = dateFormat.format(newActualDate.getTime());
         Assert.assertEquals(actual, expected);
@@ -37,6 +32,9 @@ public class StringToDateTest {
 
     @Test
     public void testYesterday() {
+        calendar = new StringToDate();
+        newCalendar = Calendar.getInstance();
+        newActualDate = newCalendar.getTime();
         String actual = calendar.convertirString("YESTERDAY");
         newCalendar.set(Calendar.DAY_OF_MONTH, newCalendar.get(Calendar.DATE) - 1);
         String expected = dateFormat.format(newCalendar.getTime());
@@ -45,6 +43,9 @@ public class StringToDateTest {
 
     @Test
     public void testTomorrow() {
+        calendar = new StringToDate();
+        newCalendar = Calendar.getInstance();
+        newActualDate = newCalendar.getTime();
         String actual = calendar.convertirString("TOMORROW");
         newCalendar.set(Calendar.DAY_OF_MONTH, newCalendar.get(Calendar.DATE) + 1);
         String expected = dateFormat.format(newCalendar.getTime());
@@ -53,6 +54,9 @@ public class StringToDateTest {
 
     @Test
     public void testTwoDaysAgo() {
+        calendar = new StringToDate();
+        newCalendar = Calendar.getInstance();
+        newActualDate = newCalendar.getTime();
         String actual = calendar.convertirString("2 days ago");
         newCalendar.set(Calendar.DATE, newCalendar.get(Calendar.DATE) - 2);
         String expected = dateFormat.format(newCalendar.getTime());
@@ -61,6 +65,9 @@ public class StringToDateTest {
 
     @Test
     public void test15MinutesFromNow() {
+        calendar = new StringToDate();
+        newCalendar = Calendar.getInstance();
+        newActualDate = newCalendar.getTime();
         String actual = calendar.convertirString("15 minutes from now");
         newCalendar.set(Calendar.MINUTE, newCalendar.get(Calendar.MINUTE) + 15);
         String expected = dateFormat.format(newCalendar.getTime());
@@ -69,6 +76,9 @@ public class StringToDateTest {
 
     @Test
     public void test5monthsFromNow() {
+        calendar = new StringToDate();
+        newCalendar = Calendar.getInstance();
+        newActualDate = newCalendar.getTime();
         String actual = calendar.convertirString("5 months from now");
         newCalendar.set(Calendar.MONTH, newCalendar.get(Calendar.MONTH) + 5);
         String expected = dateFormat.format(newCalendar.getTime());
@@ -77,6 +87,9 @@ public class StringToDateTest {
 
     @Test
     public void test3yearsAgo() {
+        calendar = new StringToDate();
+        newCalendar = Calendar.getInstance();
+        newActualDate = newCalendar.getTime();
         String actual = calendar.convertirString("3 years ago");
         newCalendar.set(Calendar.YEAR, newCalendar.get(Calendar.YEAR) - 3);
         String expected = dateFormat.format(newCalendar.getTime());
@@ -85,6 +98,9 @@ public class StringToDateTest {
 
     @Test
     public void test1dayAgo() {
+        calendar = new StringToDate();
+        newCalendar = Calendar.getInstance();
+        newActualDate = newCalendar.getTime();
         String actual = calendar.convertirString("1 day ago");
         newCalendar.set(Calendar.DATE, newCalendar.get(Calendar.DATE) - 1);
         String expected = dateFormat.format(newCalendar.getTime());
